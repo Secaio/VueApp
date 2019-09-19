4c9cd486ef7e6b1349b4416b5823e1679a6e0b84
secaio@gmail.com <123deoliveira69>
2019-09-19 16:14:09 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ import Experience from './views/Experience.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
